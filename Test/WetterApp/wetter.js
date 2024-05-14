38ec5838c380968d4db4b9889a8dc069789a2b14
MoritzVogt <mvogt@lib-it.de>
2024-05-14 21:23:11 +0200

    Ordner Struktur hinzugefügt + Wetterapp

---
+++ Test/WetterApp/wetter.js
@@ -0,0 +1,38 @@
+var city = "Stuttgart"
+
+const temp = document.querySelector(".temperature")
+var userInput = document.querySelector("#iUserInput");
+if (!userInput.value) {
+    userInput.value = city;
+}
+
+const img = document.querySelector("img")
+const currTime = document.querySelector(".time")
+
+async function getWeather(){
+    const result = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + encodeURI(city) + "&appid=94f71069f647fb7800be2f7f846f4db0" + "&units=metric");
+    const data = await result.json()
+
+    console.log(data);
+
+    let tempValue = Math.round(data.main.temp *10) / 10
+    tempValue = tempValue.toFixed(1)
+    tempValue = Number(tempValue)
+    temp.innerHTML = tempValue.toLocaleString('DE-de');
+
+    const timeElapsed = Date.now()
+
+    const today = new Date(data.dt * 1000)
+    currTime.innerHTML = today.toString();
+
+}
+
+setInterval(getWeather, 10000);
+
+document.getElementById("iUserInput").addEventListener('keydown', function (event) {
+    if (event.key === "Enter" || event.keyCode === 13) {
+        city = event.target.value
+        console.log(city);
+    }
+})
+console.log(city);
