8a3e2925ed25e874ec59026843cb646ed77ec4ec
MoritzVogt <mvogt@lib-it.de>
2024-05-16 21:00:47 +0200

    updated weather app, added more information shown to the user

--- Test/WetterApp/wetter.js
+++ Test/WetterApp/wetter.js
@@ -1,37 +1,52 @@
 var city = "Stuttgart"
-
+var iconUrl = "http://openweathermap.org/img/wn/04n@2x.png"
 const temp = document.querySelector(".temperature")
+const feelslike = document.getElementById("feelslike")
+const weatherIconDiv = document.getElementById("weatherIcon")
+var iconCode = "";
+
 var userInput = document.querySelector("#iUserInput");
 if (!userInput.value) {
     userInput.value = city;
-    setInterval(getWeather, 10000);
 }
 
 const img = document.querySelector("img")
 const currTime = document.querySelector(".time")
-
+userInput.focus();
 async function getWeather(){
     const result = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + encodeURI(city) + "&appid=94f71069f647fb7800be2f7f846f4db0" + "&units=metric");
     const data = await result.json()
-
     console.log(data);
-
+    console.log(data.weather[0])
+    iconCode = data.weather[0].icon
+    if(iconCode != "" || null){
+        iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    }
+    document.getElementById("iWeatherImg").src = iconUrl;
+    document.getElementById("iWeatherDesc").innerHTML = data.weather[0].description
     let tempValue = Math.round(data.main.temp *10) / 10
-    tempValue = tempValue.toFixed(1)
+    let feelsLike = Math.round(data.main.feels_like *10) / 10
+    console.log(tempValue)
+    tempValue = tempValue.toString()
+    feelsLike = feelsLike.toString()
     tempValue = Number(tempValue)
-    temp.innerHTML = tempValue.toLocaleString('DE-de');
+    feelsLike = Number(feelsLike)
+    temp.innerHTML = tempValue.toLocaleString('DE-de')
+    feelslike.innerHTML = feelsLike.toLocaleString('DE-de')
 
     const timeElapsed = Date.now()
 
     const today = new Date(data.dt * 1000)
-    currTime.innerText = today.toString();
+    currTime.innerHTML = today.toString();
 
 }
 
 document.getElementById("iUserInput").addEventListener('keydown', function (event) {
     if (event.key === "Enter" || event.keyCode === 13) {
         city = event.target.value
+        getWeather();
         console.log(city);
+        setInterval(getWeather, 10000);
     }
 })
 console.log(city);
